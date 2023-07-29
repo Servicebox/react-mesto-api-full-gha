8d504953833cd46a67455baf8058197e598a47ff
Кознова Тома <119004561+Servicebox@users.noreply.github.com>
2023-07-29 16:37:10 +0300

    1

---
+++ backend/app.js
@@ -0,0 +1,74 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+
+const limiter = require('./middlewares/rateLimiter');
+const cors = require('./middlewares/cors');
+
+const routeSignup = require('./routes/signup');
+const routeSignin = require('./routes/signin');
+
+const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const routeUsers = require('./routes/users');
+const routeCards = require('./routes/cards');
+
+const NotFoundError = require('./errors/NotFoundError');
+const error = require('./middlewares/error');
+
+const URL = 'mongodb://localhost:27017/mestodb ';
+
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(URL)
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('БД успешно подключена');
+  })
+  .catch(() => {
+    // eslint-disable-next-line no-console
+    console.log('Не удалось подключиться к БД');
+  });
+
+const app = express();
+
+app.use(helmet());
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogger);
+
+app.use(cors);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.use(limiter);
+
+app.use('/', routeSignup);
+app.use('/', routeSignin);
+
+app.use(auth);
+
+app.use(errorLogger);
+
+app.use('/users', routeUsers);
+app.use('/cards', routeCards);
+
+app.use((req, res, next) => next(new NotFoundError('Страницы по запрошенному URL не существует')));
+app.use(errors());
+app.use(error);
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server is running on port 3000');
+});
---
+++ backend/middlewares/cors.js
@@ -0,0 +1,46 @@
+/*
+В этом файле определен middleware для обработки CORS (Cross-Origin Resource Sharing) - механизма,
+ который позволяет веб-странице получать ресурсы с другого источника (домена),
+  отличного от источника самой страницы.
+*/
+
+/* allowedCors - массив, содержащий список разрешенных источников запросов.
+ Если источник запроса находится в этом списке, то будет разрешен доступ к ресурсам. */
+ const allowedCors = [
+  'https://servicebox.nomoredomains.rocks',
+  'http://servicebox.nomoredomains.rocks',
+  'https://localhost:3000',
+  'http://localhost:3000',
+  'https://localhost:3001',
+  'http://localhost:3001',
+];
+
+module.exports = (req, res, next) => {
+  const { origin } = req.headers; // сохраняет источник запроса в переменную
+  const { method } = req; // сохраняет тип запроса (HTTP-метод) в переменную
+  // сохраняем заголовки запроса, отправленные в предварительном запросе OPTIONS,
+  // в переменную requestHeaders.
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  // устанавливает заголовок, который разрешает передачу учетных данных
+  // (как куки или заголовки авторизации) в запросах с другого источника.
+  res.header('Access-Control-Allow-Credentials', true);
+  // проверяет, содержит ли массив allowedCors значение origin (источник запроса), если да,
+  // то устанавливает заголовок 'Access-Control-Allow-Origin', разрешая запросы с этого источника.
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+
+  return next();
+};
